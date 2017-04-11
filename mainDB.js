6adc11e9fa65e89ab14b424ece798fe5e53c35bc
Jay Cribas <feverjay@gmail.com>
2017-04-11 15:46:23 -0700

    Implement POST addTodo and PUT isComplete

--- mainDB.js
+++ mainDB.js
@@ -3,17 +3,42 @@ var connectionString = 'postgres://localhost:5432/todolist'
 var db = pgp(connectionString)
 
 
-const addTodo = () => db.one('INSERT INTO todos (todo) VALUES ($1) RETURNING *')
 
-const allTodos = () => db.any('SELECT * FROM todos ORDER BY id')
+// const allTodos = () => db.any('SELECT * FROM todos ORDER BY id')
+//
+// const addTodo = () => db.none('INSERT INTO todos (todo) VALUES ($1) RETURNING *')
+//
+// const oneTodo = () => db.one('SELECT * FROM todos WHERE id=$1')
+//
+// const isComplete = () => db.none('UPDATE todos SET isComplete = true WHERE id=$1')
+// const notComplete = () => db.none('UPDATE todos SET isComplete = false WHERE id=$1')
+//
+// const editTodo = () => db.one('UPDATE todos SET todo=$2 WHERE id=$1')
+//
+// const deleteTodo = () => db.one('DELETE from todos WHERE id=$1')
+//
+// module.exports = {addTodo, allTodos, oneTodo, isComplete, notComplete, editTodo, deleteTodo}
 
-const oneTodo = () => db.one('SELECT * FROM todos WHERE id=$1')
 
-const isComplete = () => db.none('UPDATE todos SET isComplete = true WHERE id=$1')
-const notComplete = () => db.none('UPDATE todos SET isComplete = false WHERE id=$1')
 
-const editTodo = () => db.one('UPDATE todos SET todo=$2 WHERE id=$1')
+const allTodos = 'SELECT * FROM todos ORDER BY id'
 
-const deleteTodo = () => db.one('DELETE from todos WHERE id=$1')
+const addTodo = 'INSERT INTO todos (todo) VALUES ($1) RETURNING *'
+//
+const oneTodo = 'SELECT * FROM todos WHERE id=$1'
 
-module.exports = {addTodo, allTodos, oneTodo, isComplete, notComplete, editTodo, deleteTodo}
+const isComplete = 'UPDATE todos SET iscomplete = NOT iscomplete WHERE id=$1'
+
+// const editTodo = 'UPDATE todos SET todo=$2 WHERE id=$1'
+//
+// const deleteTodo = 'DELETE from todos WHERE id=$1'
+//
+const Todo = {
+  allTodos: () => { return db.any( allTodos ) },
+  addTodo: (todo) => { return db.one( addTodo, [ todo ]) },
+  oneTodo: (todo) => { return db.one ( oneTodo, [ todo])},
+  isComplete: (id) => { return db.none( isComplete, [ id ]) }
+  // notComplete: (id) => { return db.none( notComplete, [ id ]) }
+}
+
+module.exports = { Todo }
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,48 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../mainDB')
+var Todo = require('../mainDB')
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   db.allTodos()
+//   .then(todos => {
+//     res.render('index', { title: 'Express', todos: todos });
+//   })
+// });
+//
+// router.post('/', function(req, res) {
+//   console.log('body?', req.body)
+//   const todo = req.body.todo
+//   db.addTodo(todo)
+//   .then(() => {
+//     res.json({todo: 'todo'})
+//   })
+// })
+
+
+
+
 router.get('/', function(req, res, next) {
-  db.allTodos()
+  Todo.Todo.allTodos()
   .then(todos => {
-    console.log(todos)
     res.render('index', { title: 'Express', todos: todos });
   })
 });
 
+router.post('/', function(req, res) {
+  console.log('body?', req.body)
+  const todo = req.body.todo
+  Todo.Todo.addTodo(todo)
+  .then(() =>
+    res.redirect('/'))
+  })
+
+router.put('/:id', function(req, res) {
+  const id = req.params.id
+  Todo.Todo.isComplete(id)
+  .then(() =>
+    res.redirect('/'))
+})
 
 
 module.exports = router;
