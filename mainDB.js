13429d88a18692932866f3fcf6accbd5aa1fb672
Jay Cribas <feverjay@gmail.com>
2017-04-12 09:46:53 -0700

    Implement DELETE deleteTodo

--- mainDB.js
+++ mainDB.js
@@ -4,38 +4,39 @@ var db = pgp(connectionString)
 
 
 
-// const allTodos = () => db.any('SELECT * FROM todos ORDER BY id')
+// const getAllTodos = () => db.any('SELECT * FROM todos ORDER BY id')
 //
-// const addTodo = () => db.none('INSERT INTO todos (todo) VALUES ($1) RETURNING *')
+// const createTodo = () => db.none('INSERT INTO todos (todo) VALUES ($1) RETURNING *')
 //
 // const oneTodo = () => db.one('SELECT * FROM todos WHERE id=$1')
 //
 // const isComplete = () => db.none('UPDATE todos SET isComplete = true WHERE id=$1')
 // const notComplete = () => db.none('UPDATE todos SET isComplete = false WHERE id=$1')
 //
-// const editTodo = () => db.one('UPDATE todos SET todo=$2 WHERE id=$1')
+// const updateTodo = () => db.one('UPDATE todos SET todo=$2 WHERE id=$1')
 //
 // const deleteTodo = () => db.one('DELETE from todos WHERE id=$1')
 //
-// module.exports = {addTodo, allTodos, oneTodo, isComplete, notComplete, editTodo, deleteTodo}
+// module.exports = {createTodo, getAllTodos, oneTodo, isComplete, notComplete, updateTodo, deleteTodo}
 
 
 
-const allTodos = 'SELECT * FROM todos ORDER BY id'
+const getAllTodos = 'SELECT * FROM todos ORDER BY id'
 
-const addTodo = 'INSERT INTO todos (todo) VALUES ($1) RETURNING *'
+const createTodo = 'INSERT INTO todos (todo) VALUES ($1) RETURNING *'
 
 const isComplete = 'UPDATE todos SET iscomplete = NOT iscomplete WHERE id=$1'
 
-const editTodo = 'UPDATE todos SET todo=$2 WHERE id=$1'
-//
-// const deleteTodo = 'DELETE from todos WHERE id=$1'
-//
+const updateTodo = 'UPDATE todos SET todo=$2 WHERE id=$1'
+
+const deleteTodo = 'DELETE from todos WHERE id=$1'
+
 const Todo = {
-  allTodos: () => { return db.any( allTodos ) },
-  addTodo: (todo) => { return db.one( addTodo, [ todo ]) },
+  getAllTodos: () => { return db.any( getAllTodos ) },
+  createTodo: (todo) => { return db.one( createTodo, [ todo ]) },
   isComplete: (id) => { return db.none( isComplete, [ id ]) },
-  editTodo: (id, todo) => { return db.one( editTodo, [ id, todo ]) }
+  updateTodo: (id, todo) => { return db.any( updateTodo, [ id, todo ]) },
+  deleteTodo: (id) => { return db.any( deleteTodo, [ id ]) }
 }
 
 module.exports = { Todo }
