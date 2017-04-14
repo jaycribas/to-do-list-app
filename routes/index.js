c43b81dd8a0fcc8187e138c383fe2e2eeac36701
Jay Cribas <feverjay@gmail.com>
2017-04-13 18:01:10 -0700

    Connect routes to front end

--- routes/index.js
+++ routes/index.js
@@ -12,27 +12,29 @@ router.get('/', function(req, res, next) {
 
 router.post('/create', function(req, res) {
   const todo = req.body.todo
+  console.log('body?', req.body.todo)
   Todo.createTodo(todo)
   .then(() =>
     res.redirect('/'))
   })
 
-router.put('/iscomplete/:id', function(req, res) {
+router.get('/iscomplete/:id', function(req, res) {
   const { id } = req.params
   Todo.isComplete(id)
   .then(() =>
     res.redirect('/'))
 })
 
-router.put('/update/:id', function(req, res) {
+router.post('/update/:id', function(req, res) {
   const { id } = req.params
   const { todo } = req.body
+  console.log('do I get a body?', req.body)
   Todo.updateTodo(id, todo)
   .then(() =>
     res.redirect('/'))
 })
 
-router.delete('/delete/:id', function(req, res) {
+router.get('/delete/:id', function(req, res) {
   const { id } = req.params
   Todo.deleteTodo(id)
   .then(() =>
@@ -46,15 +48,14 @@ router.put('/setRankUp/:id', function(req, res) {
   .then(() => {
     res.redirect('/')
   })
+})
 
-
-  router.put('/setRankDown/:id', function(req, res) {
-    const { id } = req.params
-    const { rank } = req.body
-    Todo.setRankDown(id, rank)
-    .then(() => {
-      res.redirect('/')
-    })
+router.put('/setRankDown/:id', function(req, res) {
+  const { id } = req.params
+  const { rank } = req.body
+  Todo.setRankDown(id, rank)
+  .then(() => {
+    res.redirect('/')
   })
 })
 
